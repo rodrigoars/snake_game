4e454189051f4af6934a56251b6a4812aeb847a3
Rodrigo Augusto Rodrigues dos Santos <rodrigoars33@gmail.com>
2020-12-29 00:28:04 -0300

    criar comida

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,10 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 snake[0] = {
     x: 8 * box,
@@ -19,6 +23,11 @@ function criarCobrinha() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -54,6 +63,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
